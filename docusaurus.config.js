87ebd69b0205aecbb178d6de82305f11ecc65a5e
Florian Lauterbach <94018411+FlorianLauterbach@users.noreply.github.com>
2024-08-26 23:55:50 +0200

    #1: updates project

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,11 +13,11 @@ const config = {
   favicon: 'img/rob18n.png',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://rob18n.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/app',
-
+  baseUrl: '/',
+  trailingSlash: false,
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'rob18n', // Usually your GitHub org/user name.
