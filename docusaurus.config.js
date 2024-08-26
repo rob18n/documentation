2831b065fb9b4662a4cbeebc58589387bdc772ed
Florian Lauterbach <94018411+FlorianLauterbach@users.noreply.github.com>
2024-08-26 23:04:35 +0200

    #1: removes broken link errors

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,7 +23,7 @@ const config = {
   organizationName: 'rob18n', // Usually your GitHub org/user name.
   projectName: 'app', // Usually your repo name.
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
